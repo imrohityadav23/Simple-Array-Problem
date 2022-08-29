ec9314d52b5e29eff151725ca3dd04cc5e13486c
ROHIT KUMAR <95360799+iamrohityadav23@users.noreply.github.com>
2022-08-29 14:40:22 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+public class Array{
+ public static void main(String []args){
+  int month [];
+month = new int[12];
+month[0] = 31;
+month[1] = 28;
+month[2] = 31;
+month[3] = 30;
+month[4] = 31;
+month[5] = 30;
+month[6] = 31;
+month[7] = 31;
+month[8] = 30;
+month[9] = 31;
+month[10] = 30;
+month[11] = 31;
+System.out.println("August has " +month[7] + "days.");
+}
+}
+
+
